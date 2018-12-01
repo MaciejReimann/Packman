5855cbbb0352c2693436696c8ab4846b71b046f8
MaciejReimann <maciej.reimann@gmail.com>
2018-12-01 21:31:20 +0100

    boilerplate reinstalled

--- webapp_front/server/index.js
+++ webapp_front/server/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 const path = require('path');
 // Take requests and data from the body
 const bodyParser = require('body-parser');
