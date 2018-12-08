9762b7c51fef52c8609f779d79beeabbe7074521
MaciejReimann <maciej.reimann@gmail.com>
2018-12-08 01:20:09 +0100

    Add button styled

--- webapp_front/client/src/components/presentation/Add.js
+++ webapp_front/client/src/components/presentation/Add.js
@@ -5,10 +5,10 @@ import '../../Add.css';
 
 const Add = () => {
     return (
-        <Link>
+        <Link to="/add">
             <div className = "add">
                 <button>
-                    +
+                <i className="fas fa-plus"></i>
                 </button>
             </div>
         </Link>        
