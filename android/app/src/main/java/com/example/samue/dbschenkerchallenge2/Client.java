b2c2b63be4a499dc9b4f3eccd9731851e20ca1ef
sammy9867 <samuelmenezes15@gmail.com>
2018-12-08 23:49:42 +0100

    Merge branch 'master' of https://github.com/sammy9867/DBSchenkerChallenge2
    # Conflicts:
    #	android/app/src/main/java/com/example/samue/dbschenkerchallenge2/DriverActivity.java

--- android/app/src/main/java/com/example/samue/dbschenkerchallenge2/Client.java
+++ android/app/src/main/java/com/example/samue/dbschenkerchallenge2/Client.java
@@ -1,5 +1,7 @@
 package com.example.samue.dbschenkerchallenge2;
 
+import java.util.ArrayList;
+
 /**
  * Created by michael on 08.12.18.
  */
@@ -8,13 +10,30 @@ public class Client {
 
     public String id;
     public String name;
-    public String address;
+    public String description;
     public String phone_number;
+    public String address;
+    public String from;
+    public String to;
+
+    public ArrayList<Parcel> parcels;
 
     public Client(String id, String name, String address, String phone_number) {
         this.id = id;
         this.name = name;
         this.address = address;
         this.phone_number = phone_number;
+    }
+
+    public Client(String id, String name, String description, String phone_number,
+                  String from, String to, ArrayList<Parcel> parcels) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.phone_number = phone_number;
+        this.address = "!address";
+        this.from = from;
+        this.to = to;
+        this.parcels  = new ArrayList<Parcel>(parcels);
     }
 }
