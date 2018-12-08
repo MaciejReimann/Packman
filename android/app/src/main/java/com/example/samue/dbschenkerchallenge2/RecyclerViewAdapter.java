6a19888d18109ae3bf55452a1d06851cf7400862
michael <urmich.m@gmail.com>
2018-12-08 18:15:11 +0100

    Recyclic View

--- android/app/src/main/java/com/example/samue/dbschenkerchallenge2/RecyclerViewAdapter.java
+++ android/app/src/main/java/com/example/samue/dbschenkerchallenge2/RecyclerViewAdapter.java
@@ -3,6 +3,7 @@ package com.example.samue.dbschenkerchallenge2;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -12,7 +13,23 @@ import org.w3c.dom.Text;
  * Created by michael on 08.12.18.
  */
 
-public class RecyclerViewAdapter {
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
@@ -23,6 +40,11 @@ public class RecyclerViewAdapter {
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            clientNameTextView = itemView.findViewById(R.id.clientNameTextView);
+            clientAddresstextView = itemView.findViewById(R.id.clientAddressTextView);
+            parentLayout = itemView.findViewById(R.id.parent_layout);
+
         }
     }
 }
