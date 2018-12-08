89b0a44c0a7bf36452b1d37f73f8bf227d46e61f
Adam Madela <amm@netcomapny.com>
2018-12-08 19:27:48 +0100

    Pakage endpoint and changes to Client endpoint

--- webapp/src/main/java/hello/domain/ClientDto.java
+++ webapp/src/main/java/hello/domain/ClientDto.java
@@ -6,9 +6,6 @@ public class ClientDto implements BaseDto<Client> {
 
     private Long id;
     private String name;
-    private String address;
-    private String packageId;
-    private String time;
     private String description;
     private String phone;
 
@@ -35,29 +32,6 @@ public class ClientDto implements BaseDto<Client> {
         this.name = name;
     }
 
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPackageId() {
-        return packageId;
-    }
-
-    public void setPackageId(String packageId) {
-        this.packageId = packageId;
-    }
-
-    public String getTime() {
-        return time;
-    }
-
-    public void setTime(String time) {
-        this.time = time;
-    }
 
     public String getDescription() {
         return description;
@@ -81,10 +55,7 @@ public class ClientDto implements BaseDto<Client> {
         setId(entity.getId());
         setName(entity.getName());
         setDescription(entity.getDescription());
-        setAddress(entity.getAddress());
-        setPackageId(entity.getPackageId());
         setPhone(entity.getPhone());
-        setTime(entity.getTime());
         return this;
     }
 
@@ -94,10 +65,7 @@ public class ClientDto implements BaseDto<Client> {
         entity.setId(getId());
         entity.setName(getName());
         entity.setDescription(getDescription());
-        entity.setAddress(getAddress());
-        entity.setPackageId(getPackageId());
         entity.setPhone(getPhone());
-        entity.setTime(getTime());
         return entity;
     }
 }
--- webapp/src/main/java/hello/model/Client.java
+++ webapp/src/main/java/hello/model/Client.java
@@ -1,12 +1,10 @@
 package hello.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
-@Table(name= "client")
+@Table(name = "client")
 public class Client implements BaseEntity {
 
     @Id
@@ -16,17 +14,37 @@ public class Client implements BaseEntity {
     @Column(name = "name")
     private String name;
 
-    @Column(name = "address")
-    private String address;
-
-    @Column(name = "packageId")
-    private String packageId;
-
-    @Column(name = "time")
-    private String time;
-
     @Column(name = "description")
     private String description;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "bfr")
+    private String bfr;
+
+    @OneToMany(mappedBy="client", fetch = FetchType.EAGER)
+    private List<Parcel> parcels;
+
+    public List<Parcel> getParcels() {
+        return parcels;
+    }
+
+    public void setParcels(List<Parcel> parcels) {
+        this.parcels = parcels;
+    }
+
+    public String getBfr() {
+        return bfr;
+    }
+
+    public void setBfr(String bfr) {
+        this.bfr = bfr;
+    }
+
+    public Long getId() {
+        return id;
+    }
 
     public String getPhone() {
         return phone;
@@ -34,21 +52,6 @@ public class Client implements BaseEntity {
 
     public void setPhone(String phone) {
         this.phone = phone;
-    }
-
-    @Column(name = "phone")
-    private String phone;
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getPackageId() {
-        return packageId;
-    }
-
-    public void setPackageId(String packageId) {
-        this.packageId = packageId;
     }
 
     public String getDescription() {
@@ -69,21 +72,5 @@ public class Client implements BaseEntity {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getTime() {
-        return time;
-    }
-
-    public void setTime(String time) {
-        this.time = time;
     }
 }
