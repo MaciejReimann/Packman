303d38de1d345545d9dd6ee1fd49c77e02db153e
sammy9867 <samuelmenezes15@gmail.com>
2018-12-08 19:10:41 +0100

    ww

--- android/app/src/main/java/com/example/samue/dbschenkerchallenge2/DriverActivity.java
+++ android/app/src/main/java/com/example/samue/dbschenkerchallenge2/DriverActivity.java
@@ -1,15 +1,16 @@
 package com.example.samue.dbschenkerchallenge2;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class DriverActivity extends AppCompatActivity {
 
+    public Context mContext = DriverActivity.this;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver);
-
-
     }
 }
