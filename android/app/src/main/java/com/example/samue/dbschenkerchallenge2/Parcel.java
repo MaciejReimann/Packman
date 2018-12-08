e7eea8d2b2c2e942bbf8e9219e2b5e795c5fe0e7
sammy9867 <samuelmenezes15@gmail.com>
2018-12-08 23:55:31 +0100

    Merge branch 'master' of https://github.com/sammy9867/DBSchenkerChallenge2
    # Conflicts:
    #	android/app/src/main/java/com/example/samue/dbschenkerchallenge2/DriverActivity.java

---
+++ android/app/src/main/java/com/example/samue/dbschenkerchallenge2/Parcel.java
@@ -0,0 +1,20 @@
+package com.example.samue.dbschenkerchallenge2;
+
+public class Parcel {
+
+    public String id;
+    public String parcelNo;
+    public String address;
+    public String deliveryTime;
+    public String eta;
+    public String status;
+
+    public Parcel(String id, String parcelNo, String address, String deliveryTime, String eta, String status) {
+        this.id = id;
+        this.parcelNo = parcelNo;
+        this.address = address;
+        this.deliveryTime = deliveryTime;
+        this.eta = eta;
+        this.status = status;
+    }
+}
