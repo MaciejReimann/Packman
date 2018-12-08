71ccee9d437461a0b44d8df397f92f2312e1ea46
Adam Madela <amm@netcomapny.com>
2018-12-08 22:47:13 +0100

    next changes on backend

--- webapp/src/main/java/hello/controllers/ClientRestController.java
+++ webapp/src/main/java/hello/controllers/ClientRestController.java
@@ -2,10 +2,7 @@ package hello.controllers;
 
 import hello.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -22,9 +19,9 @@ public class ClientRestController {
     }
 
     @ResponseBody
-    @RequestMapping(value = "/api/package", method = RequestMethod.GET, produces = "application/json")
-    public Object getParcels(HttpServletRequest request) {
-        String token = request.getHeader("token");
+    @RequestMapping(value = "/api/package/{token}", method = RequestMethod.GET, produces = "application/json")
+    public Object getParcels(HttpServletRequest request, @PathVariable(name="token") String token) {
+//        String token = request.getHeader("token");
         return clientService.getParcelsForClient(token);
     }
 
--- webapp/src/main/java/hello/controllers/MainController.java
+++ webapp/src/main/java/hello/controllers/MainController.java
@@ -1,30 +1,24 @@
 package hello.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import hello.services.NameService;
-
 @Controller
 public class MainController {
-	
-	@Autowired
-	NameService nameService;
-	
-	@RequestMapping("/")
-	public String main(Model model) {
-		return "index";
-	}
 
-	@RequestMapping("/client")
-	public String client(Model model) {
-		return "client/index";
-	}
+    @RequestMapping("/")
+    public String main(Model model) {
+        return "index";
+    }
 
-	@RequestMapping("/parcel")
-	public String parcel(Model model) {
-		return "parcel/index";
-	}
+    @RequestMapping("/client")
+    public String client(Model model) {
+        return "client/index";
+    }
+
+    @RequestMapping("/parcel")
+    public String parcel(Model model) {
+        return "parcel/index";
+    }
 }
