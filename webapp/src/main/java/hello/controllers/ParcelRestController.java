a7cc099fd4ac80801e34216136fbc15c14cd3e93
Adam Madela <amm@netcomapny.com>
2018-12-09 04:05:49 +0100

    updateEta endpoint

--- webapp/src/main/java/hello/controllers/ParcelRestController.java
+++ webapp/src/main/java/hello/controllers/ParcelRestController.java
@@ -5,6 +5,7 @@ import hello.model.Parcel;
 import hello.repositories.ParcelRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
@@ -16,15 +17,25 @@ public class ParcelRestController {
     @Autowired
     ParcelRepository parcelRepository;
 
-    @ResponseBody
     @RequestMapping(value = "/api/parcel/delivered", method = RequestMethod.POST, consumes = "application/json")
-    public void getClients(@RequestBody ParcelDto parcelDto, HttpServletResponse response) {
+    public void setDelivered(@RequestBody ParcelDto parcelDto, HttpServletResponse response) {
         Parcel parcel = parcelRepository.findOne(parcelDto.getId());
         if (parcel == null) {
             response.setStatus(HttpStatus.BAD_REQUEST.value());
         } else {
             parcel.setStatus("delivered");
             parcel.setEta("0");
+            parcelRepository.save(parcel);
+        }
+    }
+
+    @RequestMapping(value = "/api/parcel/updateEta", method = RequestMethod.POST, consumes = "application/json")
+    public void changeETA(@RequestBody ParcelDto parcelDto, HttpServletResponse response) {
+        Parcel parcel = parcelRepository.findOne(parcelDto.getId());
+        if (parcel == null || StringUtils.isEmpty(parcel.getEta())) {
+            response.setStatus(HttpStatus.BAD_REQUEST.value());
+        } else { ;
+            parcel.setEta(parcelDto.getEta());
             parcelRepository.save(parcel);
         }
     }
